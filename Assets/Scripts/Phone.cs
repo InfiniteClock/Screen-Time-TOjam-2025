2cf026ca37b7df6fb6f95c453aee104d60d3433b
InfiniteClock <156016040+InfiniteClock@users.noreply.github.com>
2025-05-09 19:36:49 -0400

    Clock working

---
+++ Assets/Scripts/Phone.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Phone : MonoBehaviour
+{
+    public int timeHours = 10;
+    public int timeMinutes = 0;
+    public string timeAMPM = "PM";
+    public bool clockActive = true;
+
+    public TextMeshProUGUI clockText;
+
+    private void Start()
+    {
+        StartCoroutine(ClockTick());
+    }
+    private void Update()
+    {
+        
+    }
+
+    private IEnumerator ClockTick(float secondsPerMin = 0.2f)
+    {
+        while (clockActive)
+        {
+            if (timeMinutes < 59)
+                timeMinutes += 1;
+            else
+            {
+                timeMinutes -= 59;
+                if (timeHours < 12)
+                {
+                    timeHours += 1;
+                    if (timeHours == 12)
+                    {
+                        if (timeAMPM == "AM")
+                            timeAMPM = "PM";
+                        else
+                            timeAMPM = "AM";
+                    }
+                }
+                else
+                {
+                    timeHours -= 11;
+
+                }
+            }
+            int minuteTens = timeMinutes / 10;
+            int minuteOnes = timeMinutes % 10;
+            clockText.text = timeHours + ":" + minuteTens + "" + minuteOnes + timeAMPM;
+            yield return new WaitForSeconds(secondsPerMin);
+        }
+    }
+}
