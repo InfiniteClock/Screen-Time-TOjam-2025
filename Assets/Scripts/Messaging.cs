79e3ca797389ab223d93f934077f744122cc4ec0
dg015 <bruno.barbosa8@yahoo.com>
2025-05-10 21:03:19 -0400

    Scriptable events and folder organization

---
+++ Assets/Scripts/Messaging.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Email;
+
+public enum Sender
+{
+    Mom,
+    Dad,
+    GF,
+    BobBobson,
+    CarlosRoomate,
+    AnthonyAnnoyingFriend,
+    DeliveryGuy,
+
+}
+
+[CreateAssetMenu(menuName = "Dialogue/Messaging")]
+public class Messaging : ScriptableObject
+{
+
+    public Sender sender;
+    public string message;
+    public string correctOption1;
+    public string correctOption2;
+    public string wrongOption;
+
+
+
+
+
+}
