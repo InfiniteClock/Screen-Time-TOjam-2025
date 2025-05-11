4afb3f15673ec70a8c6e49e10c3e7053498a2e87
InfiniteClock <156016040+InfiniteClock@users.noreply.github.com>
2025-05-11 13:45:23 -0400

    Added Credit Button to Main Menu

--- Assets/Scripts/Scene Transition/MainMenu.cs
+++ Assets/Scripts/Scene Transition/MainMenu.cs
@@ -6,9 +6,14 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public string gameScene;
-
+    public string creditsScene;
     public void PressadaButton()
     {
         SceneManager.LoadScene(gameScene);
+    }
+
+    public void PressadaOtherButton()
+    {
+        SceneManager.LoadScene(creditsScene);
     }
 }
